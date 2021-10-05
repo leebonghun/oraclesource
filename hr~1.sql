f10b001aea09e1d5506c6c19b13a8f077b735e03
LEE <>
2021-10-05 13:09:30 +0900

    hi

--- hr~1.sql
+++ hr~1.sql
@@ -470,3 +470,19 @@ where e.department_id = d.department_id and d.department_name = 'Executive';
 SELECT employee_id,last_name from employees
 where department_id in 
 (select distinct department_id from employees where last_name like '%u%');
+
+--
+CREATE TABLE indexTBL as select DISTINCT first_name, last_name , hire_date from employees;
+
+select * from indexTBL;
+
+-- 인덱스 생성 전 검색 방식 : Full
+select * from indexTBL where first_name= 'Jack';
+
+
+-- 인덱스 생성
+create index idx_indexTBL_firstname on indexTBL(first_name);
+
+
+-- 인덱스 생성 후 검색 방식 : index 검색(Range Scan)
+select * from indexTBL where first_name ='Jack';
